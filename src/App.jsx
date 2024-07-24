ae45cd0154de25d94b37b2fa8488707f340d6ba6
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2024-07-24 10:32:33 -0400

    feat(routes): Add react-router-dom, configure main.jsx, and add routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,23 @@
-// 1. Importar React y otros módulos necesarios
-import React from "react";
-import Navbar from "./Components/NavBar/Navbar";
-import AboutUs from './Components/AboutUs/AboutUs';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Posts from './pages/Posts';
+import Multimedia from './pages/Multimedia';
+import Contact from './pages/Contact';
 
 
-// 2. Crear el componente App
 const App = () => {
-  // 3. Retornar JSX
   return (
-    <div className="App">
-      <Navbar />
-      <div>
-        <div className="container text-center">
-          <div className="row">
-            <div className="col contenido1 ">
-              <div className="row">
-                TERRITORIOS INDIGENAS: UN POTENCIAL PARA UNA TRANSFORMACIÓN
-                SOSTENIBLE Y JUSTA DEL SISTEMA ALIMENTARIO
-              </div>
-
-              <div className="row">
-                <div className="col-6">
-                  <button type="button" className="w-100 btn btn-light ">
-                    Light
-                  </button>
-                </div>
-
-                <div className="col-6">
-                  <button type="button" className="w-100 btn btn-success">
-                    Success
-                  </button>
-                </div>
-              </div>
-            </div>
-
-            <div className="col"></div>
-          </div>
-        </div>
-      </div>
-    <div>
-      <AboutUs></AboutUs>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/multimedia" element={<Multimedia />} />
+      </Routes>
+    </>
   );
 };
 
-// 4. Exportar el componente
 export default App;
